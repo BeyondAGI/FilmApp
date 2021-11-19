e506fc2c79464776506667b4b5ee73d1eaa837d8
Nabil <ntadili3@gatech.edu>
2021-11-19 17:46:43 +0100

    Translation for sprint 3

--- web-react/src/routes/OrganisationFeature/PersonProfession/models/main.js
+++ web-react/src/routes/OrganisationFeature/PersonProfession/models/main.js
@@ -7,12 +7,12 @@ export let emptyItem = {
 export const columnsTable= [
   { field: 'radiatorID', header: 'Radiator ID', colGroup: 'ID' },
   { field: 'id', header: 'Neo4J UUID', hidden: true },
-  { field: 'name', header: 'Profession Name', colGroup: 'General Info', isDefault: true },
+  { field: 'name', header: 'Profession name', colGroup: 'General Info' , isDefault:true },
   { field: 'description', header: 'Description', isDefault: true },
 ]
 export const columnsForm = [
   { field: 'id', header: 'Neo4J UUID', hidden: true },
-  { field: 'radiatorID', header: 'Radiator ID', colGroup: 'ID' },
-  { field: 'name', header: 'Profession Name', colGroup: 'General Info', isDefault: true },
-  { field: 'description', header: 'Description', isDefault: true },
+  { field: 'radiatorID', header: 'Radiator ID', colGroup: 'ID' },
+  { field: 'name', header: 'Profession name', colGroup: 'General Info' , isDefault:true },
+  { field: 'description', header: 'Description', isDefault:true },
 ]
